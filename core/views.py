db4ba63ebed6808c6f5245435dd8e21be362312a
JoseVale99 <lc19160007@oaxaca.tecnm.mx>
2021-08-27 02:28:23 -0500

    moved views porfolio - porfolio views

--- core/views.py
+++ core/views.py
@@ -3,8 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 
-def portfolio(request):
-    return render(request, 'core/portfolio.html')
+
 
 def about(request):
     return render(request, 'core/about.html')
